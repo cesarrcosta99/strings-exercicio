35b3a8f7f9959659d38af950d134744dc3250168
unknown <cesarrcosta99@gmail.com>
2022-11-01 17:29:41 -0300

    Exercicio 3 feito

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,6 @@
-//Crie a const para a frase aqui
+const frase="Jorge tem uma casa verde e com portão azul, com os dizeres:\"BOAS VINDAS, mas não deixe o gato sair\""
+const novaFrase=frase.replaceAll("verde","rosa").replaceAll("azul","laranja")
+console.log("Nova frase ",novaFrase)
+const minuscula="mas não deixe o gato sair\""
+const maiuscula="Jorge tem uma casa verde e com portão azul, com os dizeres:\"BOAS VINDAS,"
+console.log(maiuscula + minuscula.toUpperCase())
