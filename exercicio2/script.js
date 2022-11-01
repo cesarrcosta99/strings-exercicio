940ffc9cbb41eb8a861d5a0c481d796b47f3121b
unknown <cesarrcosta99@gmail.com>
2022-11-01 16:30:01 -0300

    exercicio1 e exercicio2 feito

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,14 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+/* a) Remova o excesso de espaços no final da string;
+
+b) exiba, em um console.log() a quantidade de caracteres da string, antes e depois da
+ remoção dos espaços;
+
+c) Substitua os traços `________` por “sticioso” */
+
+
+const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.     ";
+ const stringSemEspaco=minhaString.trim()
+ console.log("Com espaço", minhaString.length)
+ console.log("Sem Espaço", stringSemEspaco.length)
+ const novaString="Eu não sou supersticioso, mas sou um pouco \"sticioso\""
+ console.log(novaString)
